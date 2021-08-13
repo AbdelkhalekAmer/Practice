1fd386a367a7d4c478bf6d43799ff405de8d0189
Abdelkhalek <abdelkhalekamer@outlook.com>
2021-08-13 04:06:19 +0200

    Add project files.

---
+++ Algorithms/DynamicProgramming/MaxSumArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithms.DynamicProgramming
+{
+    public static class MaxSumArray
+    {
+        public static int GetMaxSumByUsingAdditionalArray(int[] array)
+        {
+            var length = array.Length;
+            if (length is 0) return 0;
+            if (length is 1) return array[0];
+            var dp = new int[length];
+            dp[0] = Math.Max(0, array[0]);
+            dp[1] = Math.Max(dp[0], array[1]);
+            for (int i = 2; i < length; i++) dp[i] = Math.Max(dp[i - 1], array[i] + dp[i - 2]);
+            return dp[length - 1];
+        }
+
+        public static int GetMaxSumByUsingTheSameInputArray(int[] array)
+        {
+            var length = array.Length;
+            if (length is 0) return 0;
+            if (length is 1) return array[0];
+            array[0] = Math.Max(0, array[0]);
+            array[1] = Math.Max(array[0], array[1]);
+            for (int i = 2; i < length; i++) array[i] = Math.Max(array[i - 1], array[i] + array[i - 2]);
+            return array[length - 1];
+        }
+    }
+}
---
+++ Main/AlgorithmsBenchmarks/DynamicProgramming/MaxSumArrayBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms.DynamicProgramming;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+using System;
+
+namespace Main.AlgorithmsBenchmarks.DynamicProgramming
+{
+    [SimpleJob(RunStrategy.Throughput,
+        launchCount: 10,
+        warmupCount: 10,
+        targetCount: 10,
+        invocationCount: 10)]
+    [IterationsColumn, MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class MaxSumArrayBenchmarks
+    {
+        private readonly int[] data;
+
+        public MaxSumArrayBenchmarks()
+        {
+            data = new int[10000];
+            for (int i = 0; i < data.Length; i++) data[i] = new Random().Next(int.MinValue, int.MaxValue);
+        }
+
+        [Benchmark]
+        public int GetMaxSumByUsingAdditionalArray() => MaxSumArray.GetMaxSumByUsingAdditionalArray(data);
+
+        [Benchmark]
+        public int GetMaxSumByUsingTheSameInputArray() => MaxSumArray.GetMaxSumByUsingTheSameInputArray(data);
+    }
+}
